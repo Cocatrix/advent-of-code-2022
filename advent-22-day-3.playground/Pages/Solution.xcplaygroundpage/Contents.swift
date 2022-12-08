5f54e162e7eea691b55f87cd3a6d98df3f20cd53
Maxime REVEL <maxime.revel@360learning.com>
2022-12-08 05:04:52 +0100

    Solve Day 3 part 1

--- advent-22-day-3.playground/Pages/Solution.xcplaygroundpage/Contents.swift
+++ advent-22-day-3.playground/Pages/Solution.xcplaygroundpage/Contents.swift
@@ -5,10 +5,12 @@ resolvePartOne(Launcher.example)
 
 func resolvePartOne(_ input: [String]) {
     
-    // TODO: Solve
+    let splitBags: [(lhs: String, rhs: String)] = splitBags(input)
+    let priorities: [Int] = splitBags.map { getPriority($0) }
+    let sum: Int = priorities.reduce(0, { $0 + $1 })
     
     D.log(D.solution, newlines: true)
-    print()
+    print(sum)
 }
 
 func resolvePartTwo(_ input: [String]) {
@@ -17,4 +19,38 @@ func resolvePartTwo(_ input: [String]) {
     
     D.log(D.solution, newlines: true)
     print()
+}
+
+extension Character {
+    var priority: Int {
+        guard let asciiValue = asciiValue else {
+            return 0
+        }
+        let priority: UInt8 = asciiValue > 95
+            ? asciiValue - 96 // a=97 -96=1
+            : asciiValue - 38 // A=65 -38=27
+        return Int(priority)
+    }
+}
+
+func splitHalf(bag: String) -> (String, String) {
+    let halfIndex = bag.count / 2
+    let leftSide = bag.prefix(halfIndex)
+    let rightSide = bag.suffix(halfIndex)
+    return (String(leftSide), String(rightSide))
+}
+
+func findCommonChar(lhs: String, rhs: String) -> Character? {
+    return lhs.first(where: { rhs.contains($0) })
+}
+
+func splitBags(_ allBags: [String]) -> [(String, String)] {
+    return allBags.map { splitHalf(bag: $0) }
+}
+
+func getPriority(_ splitBag: (String, String)) -> Int {
+    guard let commonChar = findCommonChar(lhs: splitBag.0, rhs: splitBag.1) else {
+        return 0
+    }
+    return commonChar.priority
 }
