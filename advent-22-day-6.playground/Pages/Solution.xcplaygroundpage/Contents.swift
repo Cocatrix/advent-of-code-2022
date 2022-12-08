9ea9e8f1a5fb5ccf2ae6ab75c356d4b8009f2ada
Maxime REVEL <maxime.revel@360learning.com>
2022-12-08 12:35:26 +0100

    Solve Day 6 part 1

--- advent-22-day-6.playground/Pages/Solution.xcplaygroundpage/Contents.swift
+++ advent-22-day-6.playground/Pages/Solution.xcplaygroundpage/Contents.swift
@@ -5,10 +5,13 @@ resolvePartOne(Launcher.example)
 
 func resolvePartOne(_ input: [String]) {
     
-    // TODO: Solve
-    
+    guard let stream = input.first else {
+        return
+    }
+    let solution = getFirstIndex(of: stream, minChars: Int.minDistinctChars)
+
     D.log(D.solution, newlines: true)
-    print()
+    print(solution)
 }
 
 func resolvePartTwo(_ input: [String]) {
@@ -17,4 +20,31 @@ func resolvePartTwo(_ input: [String]) {
     
     D.log(D.solution, newlines: true)
     print()
+}
+
+extension String {
+    var hasNoSameChars: Bool {
+        guard count > 0 else { return true }
+        var copy = self
+        let first = copy.removeFirst()
+        return !copy.contains(first) && copy.hasNoSameChars
+    }
+}
+
+extension Int {
+    static let minDistinctChars: Int = 4 // for Part 1
+}
+
+func getFirstIndex(of stream: String, minChars: Int) -> Int {
+    var current: String = ""
+    for (i, char) in stream.enumerated() {
+        if current.count == minChars {
+            current.removeFirst()
+        }
+        current.append(char)
+        if current.count == minChars && current.hasNoSameChars {
+            return i + 1
+        }
+    }
+    return -1
 }
