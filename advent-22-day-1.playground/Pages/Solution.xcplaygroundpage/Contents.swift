a71a362915917fa44d05d20392291feccaf9eb6b
Maxime REVEL <maxime.revel@360learning.com>
2022-12-08 04:44:30 +0100

    Solve Day 1 part 2

--- advent-22-day-1.playground/Pages/Solution.xcplaygroundpage/Contents.swift
+++ advent-22-day-1.playground/Pages/Solution.xcplaygroundpage/Contents.swift
@@ -1,7 +1,7 @@
 /// The interesting file is this one
 import Foundation
 
-resolvePartOne(Launcher.example)
+resolvePartTwo(Launcher.example)
 
 func resolvePartOne(_ input: [String]) {
     
@@ -13,10 +13,10 @@ func resolvePartOne(_ input: [String]) {
 
 func resolvePartTwo(_ input: [String]) {
     
-    // TODO: Solve
+    let max = findMaxes(parseMatrix(input))
     
     D.log(D.solution, newlines: true)
-    print()
+    print(max)
 }
 
 func parseMatrix(_ input: [String]) -> [[Int]] {
@@ -48,4 +48,28 @@ func findMax(_ matrix: [[Int]]) -> Int {
         }
     }
     return max
+}
+
+func findMaxes(_ matrix: [[Int]]) -> Int {
+    var max1: Int = 0
+    var max2: Int = 0
+    var max3: Int = 0
+    var currentLineSum: Int = 0
+    for line in matrix {
+        currentLineSum = line.reduce(0, { $0 + $1 })
+        if currentLineSum > max3 {
+            max3 = currentLineSum
+            if max3 > max2 { // Swap
+                let tmp = max2
+                max2 = max3
+                max3 = tmp
+            }
+            if max2 > max1 { // Swap
+                let tmp = max1
+                max1 = max2
+                max2 = tmp
+            }
+        }
+    }
+    return max1 + max2 + max3
 }
